82bf660beae390e51a5f0c3c18a03e7543724be7
Joel Enikanorogbon <oenikanorogbon@diamondbank.com>
2019-02-05 19:20:01 +0100

    Initial Files

---
+++ AzeezLectures/MainClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzeezLectures
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            WhileLoop initiator = new WhileLoop();
+            initiator.WhileLoopSample();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AzeezLectures/WhileLoop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzeezLectures
+{
+    public class WhileLoop
+    {
+        public void WhileLoopSample()
+        {
+            Repeat:
+            Console.WriteLine("Supply Buzz Range:");
+            Int32.TryParse(Console.ReadLine(), out int rangeNumber);
+            if (rangeNumber == 0)
+            {
+                goto Repeat;
+            }
+            int startNumber = 1;
+            while (/* Condition */startNumber <= rangeNumber) {
+                //action
+                Console.WriteLine(startNumber);
+                //increment 
+                startNumber++;
+            }
+        }
+    }
+}
