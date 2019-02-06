ee530ba56c51fba19d799050b9af8266e48f5933
Joel Enikanorogbon <oenikanorogbon@diamondbank.com>
2019-02-06 16:19:06 +0100

    ForLoop.cs Implementation

--- AzeezLectures/ForLoop.cs
+++ AzeezLectures/ForLoop.cs
@@ -8,5 +8,35 @@ namespace AzeezLectures
 {
     public class ForLoop
     {
+        public void ForLoopSample()
+        {
+            Repeat:
+            Console.WriteLine("Supply Buzz Range:");
+            Int32.TryParse(Console.ReadLine(), out int rangeNumber);
+            if (rangeNumber == 0)
+            {
+                goto Repeat;
+            }
+
+            for (int startNumber = 1; startNumber <= rangeNumber; startNumber++)
+            {
+                if (startNumber % 5 == 0  && startNumber % 3 == 0 )
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if(startNumber % 5==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(startNumber % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(startNumber);
+                }
+            }
+        }
     }
 }
